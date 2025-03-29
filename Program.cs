33d0de91d535c129e63609e3aace2dc6e218666a
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-28 22:26:36 -0300

    fim

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿﻿const double TaxaIOFMes = 0.0038;
+const double TaxaIOFDia = 0.000082;
+
+decimal totalFatura, valorPagamentoMinimo, valorNaoPago;
+decimal valorJuros, valorIOFMes, valorIOFDia, valorProximaFatura, custoRotativo;
+double pagamentoMinimo, taxaJuros;
+
+Console.WriteLine("--- Crédito Rotativo de Cartão de Crédito ---\n");
+
+Console.Write("Valor total da fatura (R$)...: ");
+totalFatura = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Pagamento mínimo (%).........: ");
+pagamentoMinimo = Convert.ToDouble(Console.ReadLine()) / 100;
+
+Console.Write("Taxa de juros mensais (%)....: ");
+taxaJuros = Convert.ToDouble(Console.ReadLine()) / 100;
+
+valorPagamentoMinimo = totalFatura * Convert.ToDecimal(pagamentoMinimo);
+
+valorNaoPago = totalFatura - valorPagamentoMinimo;
+valorJuros = valorNaoPago * Convert.ToDecimal(taxaJuros);
+valorIOFMes = valorNaoPago * Convert.ToDecimal(TaxaIOFMes);
+valorIOFDia = valorNaoPago * Convert.ToDecimal(TaxaIOFDia * 30);
+
+valorProximaFatura = valorNaoPago + valorJuros + valorIOFMes + valorIOFDia;
+custoRotativo = valorProximaFatura - valorNaoPago;
+
+Console.WriteLine($"\nPagamento mínimo..................: {valorPagamentoMinimo:C}");
+
+Console.WriteLine("\nCaso seja pago o valor mínimo:\n");
+
+Console.WriteLine($"Valor não pago....................: {valorNaoPago:C}");
+Console.WriteLine($"Juros.............................: {valorJuros:C}");
+Console.WriteLine($"IOF mensal........................: {valorIOFMes:C}");
+Console.WriteLine($"IOF diário........................: {valorIOFDia:C}");
+
+Console.WriteLine($"\nValor a pagar na próxima fatura...: {valorProximaFatura:C}");
+Console.WriteLine($"Custo do crédito rotativo.........: {custoRotativo:C}");
